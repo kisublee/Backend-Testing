9b801b155c1082a33c3c701833f3c785b8c5e1f1
kisublee <yigi2021@gmail.com>
2022-06-24 12:12:17 -0400

    Set utils with test.

---
+++ utils/stringUtils.js
@@ -0,0 +1,25 @@
+
+
+// write a function that takes a string and 
+// number called n, and repeats the string
+// n times with a space in the middle of each time.
+
+function repeatNTimesWithSpace(string, n){
+    if(!string) return '';
+
+    const arr = new Array(n).fill(string);
+
+    return arr.join(' ');
+};
+
+
+// library  => Library
+
+function capitalizeFirstLetter(string){
+
+    if(!string) return '';
+
+    return string[0].toUpperCase() + string.slice(1);
+};
+
+module.exports = {repeatNTimesWithSpace, capitalizeFirstLetter};
