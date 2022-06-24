e03c67c2c7f02d9a813d9a8ddd1ab6cfed0b0b62
kisublee <yigi2021@gmail.com>
2022-06-24 12:12:29 -0400

    set basic routes in app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+//import code for express
+const express = require('express');
+
+// initialize app
+const app = express();
+
+const studentsController = require('./controllers/studentsController');
+const namesController = require('./controllers/namesController');
+
+app.use('/students', studentsController);
+app.use('/names', namesController);
+
+// route
+app.get('/', (request, response) => {
+    response.send("Hello world!");
+});
+
+// export app
+module.exports = app;
